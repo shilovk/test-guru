b5f57b951e7736a2ede128c4f54ca5448c35e35d
ShilovK <shilovk@gmail.com>
2019-12-15 20:21:45 +0300

    add question to answer

--- db/migrate/20191215114827_add_user_to_answer.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToAnswer < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :answers, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_114827) do
+ActiveRecord::Schema.define(version: 2019_12_15_172047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2019_12_15_114827) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "correct", default: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_answers_on_user_id"
+    t.bigint "question_id", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_114827) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "answers", "users"
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
