dc3d51494b8090f1963e67088904c478fb235b0a
ShilovK <shilovk@gmail.com>
2019-12-15 20:38:31 +0300

    add to table

--- db/migrate/20191215172047_add_question_to_answer.rb
+++ db/migrate/20191215172047_add_question_to_answer.rb
@@ -1,5 +1,5 @@
 class AddQuestionToAnswer < ActiveRecord::Migration[6.0]
   def change
-    add_reference :answers, :question, null: false, foreign_key: true
+    add_reference :answers, :question, null: false, foreign_key: {to_table: :questions}
   end
 end
