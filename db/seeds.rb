124beb88cd20d49484cc9a814d1134610cdb5aa4
ShilovK <shilovk@gmail.com>
2019-12-18 23:43:01 +0300

    rubocop fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,34 +9,34 @@
 categories_array = %w[Ruby Rails Go HTML].each_with_object([]) do |title, memo|
   memo << { title: title }
 end
-pp categories = Category.create(categories_array)
+Rails.logger.debug categories = Category.create(categories_array)
 
 tests_array = categories.each_with_object([]) do |category, memo|
   memo << { title: 'Begin', level: 1, published: true, category_id: category.id }
 end
-pp tests = Test.create(tests_array)
+Rails.logger.debug tests = Test.create(tests_array)
 
 questions_array = tests.each_with_object([]) do |test, memo|
   memo << { body: 'Is it frontend', test_id: test.id }
   memo << { body: 'Is it backend', test_id: test.id }
 end
-pp questions = Question.create(questions_array)
+Rails.logger.debug questions = Question.create(questions_array)
 
 answers_array = questions.each_with_object([]) do |question, memo|
   memo << { body: 'Yes', question_id: question.id }
   memo << { body: 'No', correct: true, question_id: question.id }
 end
-pp Answer.create(answers_array)
+Rails.logger.debug Answer.create(answers_array)
 
 users_array = []
 users_array << { name: 'Adm', email: 'admin@admin.ru', password: 'adm', admin: true }
 users_array << { name: 'Tom', email: 'tom@tom.ru', password: 'tom' }
 users_array << { name: 'Jerry', email: 'jerry@jerry.ru', password: 'jerry' }
-pp users = User.create(users_array)
+Rails.logger.debug users = User.create(users_array)
 
 results_array = users.each_with_object([]) do |user, memo|
   tests.each do |test|
     memo << { value: rand(1..2), test_id: test.id, user_id: user.id }
   end
 end
-pp Result.create(results_array)
+Rails.logger.debug Result.create(results_array)
