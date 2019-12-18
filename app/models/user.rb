57d153d1d51bb4ab1d83f8b337bac39d7c7aead5
ShilovK <shilovk@gmail.com>
2019-12-18 21:09:05 +0300

    update model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.where(id: results.where(level: level).pluck(:id))
+    Test.where(level: level, id: results.pluck(:test_id))
   end
 end
