671d73c227a4bf45c2638ac5121f5f7df6085774
ShilovK <shilovk@gmail.com>
2019-12-18 23:13:48 +0300

    add alternate ways

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,12 @@ class Test < ApplicationRecord
   def self.titles_by_category(title)
     Category.find_by(title: title).tests.pluck(:title).sort
   end
+
+  def self.titles_by_category_alternate(title)
+    Test.find_each(batch_size: 100)
+        .joins(:categories)
+        .where(tests: { category_id: Category.find_by(title: title).id })
+        .order('tests.title')
+        .pluck('tests.title')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   def tests_by_level(level)
     Test.where(level: level, id: results.pluck(:test_id))
   end
+
+  def tests_by_level_alternate(level)
+    Test.find_each(batch_size: 100)
+        .joins(:resuts)
+        .where(tests: { level: level })
+        .where(results: { test_id: 'tests.id', user_id: id })
+  end
 end
