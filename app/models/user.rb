aee2d3d3a18dda2a6b577dee4f8d8938c5db694c
ShilovK <shilovk@gmail.com>
2019-12-18 20:34:58 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    Test.where(user: self, level: level)
+    test_ids = Result.where(user_id: id, level: level).pluck(:id)
+    Test.where(id: test_ids)
   end
 end
