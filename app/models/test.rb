da06763c0df859bc4527f792d89761fa3b0eb41a
ShilovK <shilovk@gmail.com>
2019-12-19 12:07:42 +0300

    fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
   def self.titles_by_category(title)
     category_id = Category.find_by(title: title).id
-    joins("JOIN categories ON (tests.category_id = categories.id AND categories.id = #{category_id})")
+    joins("JOIN categories
+           ON (tests.category_id = categories.id
+           AND categories.id = #{category_id})")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   def tests_by_level(level)
     Test.where(level: level)
-        .joins("JOIN results ON (tests.id = results.test_id AND results.user_id = #{id})")
+        .joins("JOIN results
+                ON (tests.id = results.test_id
+                AND results.user_id = #{id})")
   end
 end
