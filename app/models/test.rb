e754e2b7f75f875139fa53b22e338506841783bb
ShilovK <shilovk@gmail.com>
2019-12-22 16:05:54 +0300

    before fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category, optional: true
-  belongs_to :author, class_name: 'User', inverse_of: :created_tests,
-             optional: true
+  belongs_to :author, class_name: 'User', inverse_of: :created_tests, optional: true
   has_many :questions, dependent: :destroy
   has_many :test_users, dependent: :destroy
   has_many :users, through: :test_users
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :test_users, dependent: :destroy
   has_many :passed_tests, through: :test_users, source: 'test'
-  has_many :created_tests, class_name: 'Test', inverse_of: :author,
-           foreign_key: 'author_id', dependent: :nullify
+  has_many :created_tests, class_name: 'Test', inverse_of: :author, foreign_key: 'author_id', dependent: :nullify
 
   def passed_tests_by_level(level)
     passed_tests.where(level: level)
