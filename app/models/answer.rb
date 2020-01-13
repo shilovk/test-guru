3cd16193727d9e4af308fe124d93238c8171e61a
ShilovK <shilovk@gmail.com>
2020-01-13 21:39:39 +0300

    fix count of answers for question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,11 +2,12 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_max_answers_for_question
+  validate :validate_max_answers_for_question, on: %i[create update]
 
   scope :correct, -> { where(correct: true) }
 
   def validate_max_answers_for_question
-    errors.add(:question, 'have already enough answers(1..4)') if question.answers.count >= 4
+    number_of_records = new_record? ? 4 : 5
+    errors.add(:question, 'have already enough answers(1..4)') if question.answers.count >= number_of_records
   end
 end
