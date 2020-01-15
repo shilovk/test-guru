58e3f11b6050bf979aba785e89d635500ac715b4
ShilovK <shilovk@gmail.com>
2020-01-15 13:40:15 +0300

    add files

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,48 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index show create]
+  before_action :find_question, only: %i[show delete]
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    exception.model == 'Question' ? rescue_with_question_not_found(exception.id)  : raise
+  end
+
+  def index
+    @questions = @test.questions
+
+    render plain: @questions.inspect
+  end
+
+  def create
+    question = Question.create(question_params)
+
+    render plain: question.inspect
+  end
+
+  def show
+    render plain: @question.inspect
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to test_questions_path, notice: 'Question was successfuly deleted'
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found(id)
+    render plain: "Question #{id} was not found"
+  end
+
+  def find_question
+    @question = @test.questions.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:test_id, :body)
+  end
+end
