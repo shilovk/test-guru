9d5f247619efbd3b91ac16d13a8613969987294c
ShilovK <shilovk@gmail.com>
2020-01-16 13:26:29 +0300

    fix old answer validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,12 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
-  validate :validate_max_answers_for_question, on: %i[create update]
+  validate :validate_max_answers_for_question, on: :create
 
   scope :correct, -> { where(correct: true) }
 
   def validate_max_answers_for_question
-    number_of_records = new_record? ? 4 : 5
-    errors.add(:question, 'have already enough answers(1..4)') if question.answers.count >= number_of_records
+    errors.add(:question, 'have already enough answers(1..4)') if question.answers.count >= 4
   end
 end
