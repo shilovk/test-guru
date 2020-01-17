da46870ac91f00877b23e906533ad1b8835db0b2
ShilovK <shilovk@gmail.com>
2020-01-17 12:58:40 +0300

    all done

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,4 +1,11 @@
 module TestsHelper
+
+  TEST_LEVELS = { 0 => :easy, 1 => :normal, 3 => :hard }.freeze
+
+  def test_level(test)
+    TEST_LEVELS[test.level] || :hero
+  end
+
   def test_header(test)
     test.persisted? ? 'Edit Test' : 'Create New Test'
   end
