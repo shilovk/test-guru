40b612c8c956a5ef1ca7f97e495db72cab296a5b
ShilovK <shilovk@gmail.com>
2020-01-20 20:42:00 +0300

    done

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,7 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_current_question, on: %i[create update]
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
@@ -34,12 +34,12 @@ class TestPassage < ApplicationRecord
 
   def before_validation_set_current_question
     self.current_question = next_question if test.present?
-
-    # self.current_question = test.questions.first if test.present?
   end
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort || false
+    return false if answer_ids.nil?
+
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
@@ -47,8 +47,7 @@ class TestPassage < ApplicationRecord
   end
 
   def next_questions
-    next_questions = test.questions
-    next_questions.where('id > ?', current_question.id) if current_question.present?
+    current_question.present? ? test.questions.where('id > ?', current_question.id) : test.questions
   end
 
   def next_question
