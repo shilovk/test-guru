2276c61c692f170aab630be351a1d526d3ad528f
ShilovK <shilovk@gmail.com>
2020-01-23 00:35:15 +0300

    using rails auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  include Auth
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, class_name: 'Test', foreign_key: :user_id
   has_many :created_tests, class_name: 'Test', inverse_of: :author, foreign_key: 'author_id', dependent: :nullify
+
+  has_secure_password
 
   def passed_tests_by_level(level)
     test_passages.where(level: level)
