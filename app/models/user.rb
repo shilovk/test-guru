f088bda63f517b63a33b116a6c646e77e13356ee
ShilovK <shilovk@gmail.com>
2020-01-22 23:45:44 +0300

    start auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, class_name: 'Test', foreign_key: :user_id
@@ -11,5 +13,15 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
+  end
+
+  def authenticate(password_string)
+    digest(password_string) == password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
