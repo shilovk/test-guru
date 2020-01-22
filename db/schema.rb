df7a6a37fee01be0589c27e732dc532e3f46d8f6
ShilovK <shilovk@gmail.com>
2020-01-22 23:20:22 +0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_192655) do
+ActiveRecord::Schema.define(version: 2020_01_22_201857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_01_19_192655) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digist"
   end
 
   add_foreign_key "answers", "questions"
