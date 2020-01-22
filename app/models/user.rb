439087f010482c34742d217ee8878eaf95b496e8
ShilovK <shilovk@gmail.com>
2020-01-23 00:31:03 +0300

    hand mate auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  include Auth
+
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, class_name: 'Test', foreign_key: :user_id
   has_many :created_tests, class_name: 'Test', inverse_of: :author, foreign_key: 'author_id', dependent: :nullify
-
-  validates :email, presence: true # , email: true
 
   def passed_tests_by_level(level)
     test_passages.where(level: level)
@@ -13,15 +11,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == password_digest ? self : false
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_204637) do
+ActiveRecord::Schema.define(version: 2020_01_22_210217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2020_01_22_204637) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digist"
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
