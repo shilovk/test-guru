69c431a6f978af2391d3ee0ed3eef365d0f0d63f
ShilovK <shilovk@gmail.com>
2020-01-28 23:21:45 +0300

    some done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,10 +7,19 @@ module ApplicationHelper
     "https://github.com/#{author}/#{repo}"
   end
 
+  def flash_class(type)
+    case type
+    when :notice then 'alert alert-info'
+    when :success then 'alert alert-success'
+    when :error then 'alert alert-danger'
+    when :alert then 'alert alert-warning'
+    end
+  end
+
   def show_flash(flash)
     content_tag :p do
       flash.each do |type, message|
-        concat(content_tag(:p, message, class: "flash #{type}"))
+        concat(content_tag(:div, message, class: "alert #{flash_class(type.to_sym)}", role: 'alert'))
       end
     end
   end
