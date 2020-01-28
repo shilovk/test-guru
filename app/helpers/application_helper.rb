42f9e466114d2f3156ac88bdebc1e026bfcab316
ShilovK <shilovk@gmail.com>
2020-01-29 01:06:47 +0300

    all done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,7 +19,19 @@ module ApplicationHelper
   def show_flash(flash)
     content_tag :p do
       flash.each do |type, message|
-        concat(content_tag(:div, message, class: "alert #{flash_class(type.to_sym)}", role: 'alert'))
+        concat(
+          content_tag(
+            :div,
+            (
+              message +
+              button_tag(type: 'button', class: 'close', 'data-dismiss': 'alert', 'aria-label': 'Close') do
+                content_tag(:span, "&times;".html_safe, 'aria-hidden': 'true')
+              end
+            ).html_safe,
+            class: "alert #{flash_class(type.to_sym)} alert-dismissible fade show",
+            role: 'alert'
+          )
+        )
       end
     end
   end
