baad1d4eecccfb7096064a73a66789d05cc55188
ShilovK <shilovk@gmail.com>
2020-01-28 21:09:36 +0300

    begin homework

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,8 +19,8 @@ const imagePath = (name) => images(name, true);
 
 import '../src/application';
 
-import 'bootstrap';
+import './bootstrap_custom';
 
 document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
 });
