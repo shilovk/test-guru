6dca946480cbb4dbcdccac208199f449e361ed3b
ShilovK <shilovk@gmail.com>
2020-01-30 03:11:33 +0300

    fixes

--- config/initializers/dotenv.rb
+++ config/initializers/dotenv.rb
@@ -1,1 +1,5 @@
-Dotenv.require_keys('GITHUB_CLIENT_ID', 'GITHUB_CLIENT_SECRET', 'GIST_ACCESS_TOKEN')
+# frozen_string_literal: true
+
+Dotenv.require_keys('GITHUB_CLIENT_ID',
+                    'GITHUB_CLIENT_SECRET',
+                    'GIST_ACCESS_TOKEN')
