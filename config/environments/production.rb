4b039edf8b6c12a66a3374a379d7edbde21b45a4
ShilovK <shilovk@gmail.com>
2020-02-08 21:24:32 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'arcane-mesa-60757.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings =  {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['SMTP_USERNAME'],
-    password:  EMV['SMTP_PASSWORD'],
-    authentication: 'plain',
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
     enable_starttls_auto: true
   }
   # Ignore bad email addresses and do not raise email delivery errors.
