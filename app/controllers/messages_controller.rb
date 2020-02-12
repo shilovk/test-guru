45b437f01a3fbdcb559661665127f34af41a5baf
ShilovK <shilovk@gmail.com>
2020-02-12 22:07:53 +0300

    fix credo again

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
-  # frozen_string_literal: true
+  skip_before_action :authenticate_user!, only: :contact
 
   def contact
     if params[:text].present? && params[:email].present?
