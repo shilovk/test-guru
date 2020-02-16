73d06266e4624fe20cf5dc38587c23bf662304e9
ShilovK <shilovk@gmail.com>
2020-02-16 18:06:12 +0300

    fix

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -4,7 +4,7 @@ class Rule < ApplicationRecord
   has_many :badge_rules
   has_many :badges, through: :badge_rules
 
-  # validates :name, :type, :need_pass, :attempt, precense: true
-  # validates :need_pass, :attempt, numericality: { only_integer: true }
-  # validates :name, uniqueness: { case_sensitive: false }
+  validates :name, :type, :need_pass, :attempt, presence: true
+  validates :need_pass, :attempt, numericality: { only_integer: true }
+  validates :name, uniqueness: { case_sensitive: false }
 end
